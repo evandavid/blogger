abee7f6e1628fa922131e53ab4eb6a1a3d38f929
Evan david kristian <ev.kristian@gmail.com>
2014-12-22 06:17:40 +0700

    additional

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
+require 'elasticsearch/model'
+
 class Post < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
